8aa075a568679895f733958dbfc66de5f3b8117f
zhemucharwood <163925480+zhemucharwood@users.noreply.github.com>
2024-08-21 19:55:59 +1000

    Add files via upload

---
+++ Netflix Stock Price Perdiction/Create a python function .py
@@ -0,0 +1,48 @@
+# This Function can be called from any from any front end tool/website(e.g Xboot)
+import pandas as pd
+from re import IGNORECASE
+from xgboost import XGBRegressor
+
+
+def FunctionPredictResult(InputData):
+    import pandas as pd
+    Num_Inputs=InputData.shape[0]
+
+    # Appending the new data with the Training data
+    DataForML=pd.read_pickle('DataForMLNetflixData.pkl')
+    #InputData=InputData.append(DataForML, ignore_index=True)
+    InputData = pd.concat([InputData, DataForML], ignore_index=True)
+
+    # Generating dummy variables for rest of the nominal variables
+    InputData=pd.get_dummies(InputData)
+
+    # Maintaining the same order of columns as it was during the model training
+    Predictors=['Open','High','Low']
+
+    # Generating the input values to the model
+    X=InputData[Predictors].values[0:Num_Inputs]
+
+
+
+    # Loading the Function from pickle file
+    import pickle
+
+    with open('Final_XGB_ModelNetflixData.pkl', 'rb') as fileReadStream:
+        PredictionModel=pickle.load(fileReadStream)
+        # Close the filestream!
+        fileReadStream.close()
+
+    # Genrating Predictions
+    Prediction=PredictionModel.predict(X)
+    PredictionResult=pd.DataFrame(Prediction, columns=['Prediction'])
+    return(PredictionResult)
+
+
+# Calling the function for some new data
+NewNetflixData=pd.DataFrame(data=[[262,267.899994,250.029999],[247.699997,266.700012,245]],columns=('Open','High','Low'))
+
+print(NewNetflixData)
+
+# Calling the Function for prediction and print.
+FunctionPredictResult(InputData=NewNetflixData)
+print(FunctionPredictResult(InputData=NewNetflixData))
---
+++ Netflix Stock Price Perdiction/Tkinter package.py
@@ -0,0 +1,66 @@
+# This Function can be called from any from any front end tool/website(e.g Xboot)
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+
+
+# Add Title and bring in the data
+class StockPricePredictionApp:
+
+    def __init__(self, master):
+        self.master = master
+        self.master.title('NFLX Price Prediction')
+        self.data = pd.read_csv('NFLX.csv')
+        self.sliders = []
+
+        # Preprocess the data of read data in columns
+        self.data = self.preprocess_data(self.data)
+        self.features = ['Open','High','Low','Close']
+
+        self.X = self.data.drop('Close', axis=1).values
+        self.y = self.data['Close'].values
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
+
+        self.model = XGBRegressor()
+        self.model.fit(self.X_train, self.y_train)
+
+        self.create_widgets()
+
+    #Drop what i don't want.
+    def preprocess_data(self, data):
+        # Delet 'Date', 'Adj Close', and 'Volume'
+        data = data.drop(['Date', 'Adj Close', 'Volume'], axis=1, errors='ignore')
+
+        for column in data.columns:
+            if data[column].dtype == 'object':
+                data = data.drop(column, axis=1)
+
+        return data
+
+    def create_widgets(self):
+        for i, column in enumerate(self.data.columns[:-1]):
+            label = tk.Label(self.master, text=column + ': ')
+            label.grid(row=i, column=0)
+            current_val_label = tk.Label(self.master, text='0.0')
+            current_val_label.grid(row=i, column=2)
+            slider = ttk.Scale(self.master, from_=self.data[column].min(), to=self.data[column].max(), orient="horizontal",
+                               command=lambda val, label=current_val_label: label.config(text=f'{float(val):.2f}'))
+            slider.grid(row=i, column=1)
+            self.sliders.append(slider)
+
+        predict_button = tk.Button(self.master, text='Predict Price', command=self.predict_price)
+        predict_button.grid(row=len(self.data.columns[:-1]), columnspan=3)
+
+    def predict_price(self):
+        inputs = [float(slider.get()) for slider in self.sliders]
+        price = self.model.predict([inputs])
+        messagebox.showinfo('Predicted Price', f'The predicted NFLX Close stock price is ${price[0]:.2f}')
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = StockPricePredictionApp(root)
+    root.mainloop()
